2cbb8300b075cd0122da7f041cf89a2e44996380
Cristiano Lopes <jirous@gmail.com>
2021-08-10 10:55:39 -0300

    mult language implementation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,76 @@
+  //test navigator language
+
+  var userLang = navigator.language || navigator.userLanguage; 
+  //alert ("O idioma do navegador é: " + userLang.substr(0,2));
+
+
+
+  // run function after page load :: get/set localstorage value and run function
+  document.addEventListener("DOMContentLoaded", function(event) {
+      var appLang = localStorage.getItem('lang');
+
+      if (userLang.substr(0,2)=='pt' && appLang === null)
+          appLang='pt';
+      else if (appLang === null)   
+          appLang='en';
+      
+      localStorage.setItem('lang', appLang); // update local-storage
+
+      // fun contentUpdate function with value from local-storage - en, ru..
+      contentUpdate(appLang); 
+
+      // select radiobutton which has data-value == local storage value
+     // document.querySelector('[data-value="'+appLang+'"]').checked = true;
+  
+  });
+
+  // change innerhtml on radiobtn click
+  function changeLang(langVal){
+  // set local-storage lang value from value given in onchange="changeLang(value)"
+  localStorage.setItem('lang', langVal);
+
+  // fun contentUpdate function with value from onchange="changeLang(value)"
+  contentUpdate(langVal);
+  }
+
+  // content/innerhtml update/assign
+  function contentUpdate(cl){
+     // alert(cl);
+  // get language json file
+  fetch('.src/lang/'+cl+'.json')
+      .then(response=> response.json())
+      .then(langJSON => {
+                  // get current langage contents in array
+      //var currLang = [Object.keys(langJSON)][1],
+      //var currLang = JSON.parse(langJSON);
+      // get current language content array length
+      langCont = Object.keys(langJSON).length; //Object.entries(currLang).length;
+      
+      //alert(langCont);
+
+      for(let i = 0; i < langCont; i++){
+      // get selectors which has .langchange classes
+      var getSelector = document.querySelectorAll('.langchange')[i],
+          // get data-key attribute from .langchange class selectors
+          getAttr = getSelector.getAttribute('data-key');
+
+      // test if the selector is image
+      if(getSelector.nodeName.toLowerCase() === 'img'){
+          getSelector.src='src/img/'+langJSON[getAttr];
+      }else{
+      // assign the data-key value from current language array to the .langchange[data-key] selector
+      getSelector.innerHTML = langJSON[getAttr];
+      }
+  }
+
+      })
+
+
+
+  }
+  
+  
+  
   // SWIPERJS
    var swiper = new Swiper(".card-players", {
      slidesPerView: 3,
